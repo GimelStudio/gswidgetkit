513318ab36e5709382217d1aa3c52f2c55d5e5d0
Noah Rahm <correctsyntax@yahoo.com>
2021-06-24 19:15:53 -0500

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
   name='gswidgetkit',
   packages=['gswidgetkit', 'gswidgetkit.icons'],
-  version='0.1.0',
+  version='0.1.1',
   license='Apache 2.0',
   description='Custom wxPython widgets created for use in Gimel Studio, highly inspired by Blender and Sketch widgets',
   long_description_content_type="text/markdown",
